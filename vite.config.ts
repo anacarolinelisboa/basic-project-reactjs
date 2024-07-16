80af33be2e5b7f11ce0f08e7d015c5213bf4f268
Ana C <carol.ndj3@gmail.com>
2024-07-16 00:41:47 -0300

    gitpages 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    proxy: {
+      '/ws': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        ws: true
+      }
+    }
+  },
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  }
 })
